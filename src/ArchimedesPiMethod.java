ad0eb3dbb0dcabd7f96a348818e9815cabb1d453
WunderWyrm <samueltheiss707@gmail.com>
2023-01-29 23:57:25 -0800

    Completed Assignment 002

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,28 @@
+/**
+ *
+ * @author Trevor Hatman
+ * @author Samuel Theiss
+ *
+ * @since Version 1.0
+ *
+ */
+
 import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
 
+        //Prompts user for sides to estimate by.
+        System.out.printf("How many sides?: ");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        double angleB = 360.0 / n;
+        double angleA = angleB / 2;
+        double sideS = 2 * Math.sin(Math.toRadians(angleA));
+        double perimeter = sideS * n;
+        double estimate = perimeter / 2;
+
+        System.out.printf("Estimate of PI is: %.10f%n", estimate);
     }
 }
